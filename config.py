31f4524ea1946f15a1f1e9a9604059bc10ad2f6a
labestia2080 <100966021+reymichel2009@users.noreply.github.com>
2022-04-21 17:21:28 -0400

    Add files via upload

---
+++ config.py
@@ -0,0 +1,15 @@
+BOT_TOKEN = '5355691004:AAFE6Qv2Kd27Pi13zvhvVE2uMyguRFlr9ps' #token de tu bot
+API_ID = 17158127 # lo creas en https://my.telegram.org/
+API_HASH = 'd1a5974070430293b64fde5339591447' # lo creas en https://my.telegram.org/
+OWNER = [5068280913]
+
+# DATOS DEL MOODLE
+USUARIO = 'reymichel200921'
+MOODLE_PASSWORD = 'Martinez200921*'
+USUARIO_ID =  5 #Tu id de la nube
+ZIP_MB = 250
+MOODLE_URL = 'aulacened.uci.cu'
+
+# CUENTA DE MEGA - este bot sirve par mega tambien
+GMAIL_MEGA = '@gmail.com' 
+PASS_MEGA = 'contraseña'
